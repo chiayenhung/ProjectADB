0d53ac4185da2b42f646fde090780f0b48ab0699
hodalalala <hodalalala@gmail.com>
2012-11-29 01:59:40 -0500

    initial

---
+++ ProjectADB/src/adb/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package adb;
+
+/**
+ * @author ching-yingyang
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
